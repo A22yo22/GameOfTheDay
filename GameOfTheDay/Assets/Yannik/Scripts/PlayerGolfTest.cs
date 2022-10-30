ac25aad96d9ffa58755323d7cfe28a4b81c25bce
Unnamed_DEV <a22yo22io@gmail.com>
2022-10-30 16:32:02 +0100

    Yannik
    Player controller
    Level folder

--- GameOfTheDay/Assets/Yannik/Scripts/PlayerGolfTest.cs
+++ GameOfTheDay/Assets/Yannik/Scripts/PlayerGolfTest.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class PlayerGolfTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float force;
+
+    public bool canShot;
+    public float maxShotDistance;
+    public float minVelocety;
+
+    public LayerMask groundLayer;
+    public Transform playerCamFollowBox;
+    public Camera cam;
+
+
+    Rigidbody rb;
+
+    private void Start()
     {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(canShot)
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            
+            
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
+            {
+                if(Vector3.Distance(transform.position, hit.point)  <= maxShotDistance)
+                {
+                    playerCamFollowBox.gameObject.SetActive(true);
+
+                    CalculateForce(hit);
+                    playerCamFollowBox.position = hit.point;
+
+                    if(Input.GetMouseButtonDown(0))
+                    {
+                        rb.AddForce((transform.position - hit.point).normalized * force, ForceMode.Impulse);
+                    }
+                }
+                else
+                {
+                    playerCamFollowBox.gameObject.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            CalculateSpeed();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CalculateForce(RaycastHit hit2)
     {
-        
+        force = Mathf.Lerp(0, 5, Mathf.InverseLerp(0, maxShotDistance, Vector3.Distance(transform.position, hit2.point)));
+    }
+
+    public void CalculateSpeed()
+    {
+        if(rb.velocity.x <= minVelocety && rb.velocity.y <= minVelocety && rb.velocity.z <= minVelocety)
+        {
+            rb.velocity = Vector3.zero;
+            canShot = true;
+            playerCamFollowBox.gameObject.SetActive(true);
+        }
+        else
+        {
+            canShot = false;
+            playerCamFollowBox.gameObject.SetActive(false);
+        }
     }
 }
